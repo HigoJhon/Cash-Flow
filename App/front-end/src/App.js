a63385e938a2b21ef820e52b1d06c6a98c8eaf97
higo jhon <higojhon98@gmail.com>
2024-05-02 16:15:04 -0300

    Criando a pagina de login

---
+++ App/front-end/src/App.js
@@ -0,0 +1,15 @@
+import { Routes, Route } from 'react-router-dom';
+import Login from './pages/login';
+import Home from './pages/home';
+
+
+function App() {
+  return (
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
+  );
+}
+
+export default App;
---
+++ App/front-end/src/pages/login.jsx
@@ -0,0 +1,70 @@
+import { useState } from "react";
+import "../style/pages/login.css"
+import { Navigate } from "react-router-dom";
+
+function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [isLogged, setIsLogged] = useState(false);
+
+  const login = async (e) => {
+    e.preventDefault();
+
+    try {
+      if (email && password) {
+        setIsLogged(true);
+      }
+    }
+    catch (error) {
+      console.error(error);
+    }
+  }
+
+  if (isLogged) return <Navigate to="/home" />;
+
+  return (
+    <>
+      <section className="user-login-area">
+        <form>
+          <h1>Login</h1>
+          <label htmlFor="email-input">
+            <input
+              type="email"
+              className="login_input"
+              placeholder="Login"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </label>
+          <label htmlFor="password-input">
+            <input
+              type="password"
+              className="login_input"
+              placeholder="Senha"
+              visible="false"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </label>
+          <a href="/">Recuperar senha</a>
+          <button 
+            className="login_button"
+            type="submit"
+            onClick={(e) => login(e)}
+          >
+            Entrar
+          </button>
+          <br />
+          <p>
+            Não tem uma conta? <a href="/">Crie uma</a>
+          </p>
+        </form>
+      </section>
+      {/* <h1>
+        Imagem de <a href="https://br.freepik.com/psd-gratuitas/ed-ilustracao-de-criptomoeda-com-monitor-e-grafico_29014159.htm">Freepik</a>
+      </h1> */}
+    </>
+  );
+}
+
+export default Login;
